a9e26ebf9497b825d67aa6f694dc99072cdae955
Piyush Dubey <piyushdubey924@gmail.com>
2026-07-05 16:22:21 +0530

    Data types in C#

---
+++ Datatypes/Program.cs
@@ -0,0 +1,19 @@
+﻿// to find minimum or maximum value of a data type we can use MinValue or Maxvalue property.
+class Program{
+    public static void Main(String []args){
+        Console.WriteLine(int.MinValue);
+        Console.WriteLine(int.MaxValue);
+        Console.WriteLine(sbyte.MinValue);
+        Console.WriteLine(sbyte.MaxValue);
+        Console.WriteLine();
+
+        float a = 292929.00999f;
+        double b = 958580999.0009d;
+        decimal c = 0334999.758745m;
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        Console.ReadLine();
+    }
+}
