a6c0fb74c824611986988d88a9af754e703fa11b
Piyush Dubey <piyushdubey924@gmail.com>
2026-07-16 19:29:53 +0530

    jump statements

--- Statements/Program.cs
+++ Statements/Program.cs
@@ -134,54 +134,114 @@
         // }
 
         // C# allows the switch case constructed to be nested as well 
-        Console.WriteLine("Enter what you would like to eact \"veg\" or \"non-veg\" ");
-        string choice = Console.ReadLine();
-        switch(choice){
-            case "veg" :
-            Console.WriteLine("Enter the choice from this list {paneer,masroom,dal-makhni,razma}");
-            string veg_item = Console.ReadLine();
-            switch(veg_item){
-                case "paneer" :
-                Console.WriteLine($"your choice is {veg_item}");
-                break;
-                case "dal-makhni" :
-                Console.WriteLine($"your choice is {veg_item}");
-                break;
-                case "razma" :
-                Console.WriteLine($"your choice is {veg_item}");
-                break;
-                case "masroom" :
-                Console.WriteLine($"your choice is {veg_item}");
-                break;
-                default :
-                Console.WriteLine("This veg-item is not available");
-                break;
-            }
-            break;
-            case "non-veg" :
-            Console.WriteLine("Enter the choice from this list {mutton,chicken,fish,egg}");
-            string item = Console.ReadLine();
-            switch(item){
-                case "mutton" :
-                Console.WriteLine($"your choice is {item}");
-                break;
-                case "chicken" :
-                Console.WriteLine($"your choice is {item}");
-                break;
-                case "fish" :
-                Console.WriteLine($"your choice is {item}");
-                break;
-                case "egg" :
-                Console.WriteLine($"your choice is {item}");
-                break;
-                default :
-                Console.WriteLine("This item is not available");
-                break;
-            }
-            break;
-            default :
-            Console.WriteLine("Please enter the exact word veg  or  non-veg");
-            break;
+        // Console.WriteLine("Enter what you would like to eact \"veg\" or \"non-veg\" ");
+        // string choice = Console.ReadLine();
+        // switch(choice){
+        //     case "veg" :
+        //     Console.WriteLine("Enter the choice from this list {paneer,masroom,dal-makhni,razma}");
+        //     string veg_item = Console.ReadLine();
+        //     switch(veg_item){
+        //         case "paneer" :
+        //         Console.WriteLine($"your choice is {veg_item}");
+        //         break;
+        //         case "dal-makhni" :
+        //         Console.WriteLine($"your choice is {veg_item}");
+        //         break;
+        //         case "razma" :
+        //         Console.WriteLine($"your choice is {veg_item}");
+        //         break;
+        //         case "masroom" :
+        //         Console.WriteLine($"your choice is {veg_item}");
+        //         break;
+        //         default :
+        //         Console.WriteLine("This veg-item is not available");
+        //         break;
+        //     }
+        //     break;
+        //     case "non-veg" :
+        //     Console.WriteLine("Enter the choice from this list {mutton,chicken,fish,egg}");
+        //     string item = Console.ReadLine();
+        //     switch(item){
+        //         case "mutton" :
+        //         Console.WriteLine($"your choice is {item}");
+        //         break;
+        //         case "chicken" :
+        //         Console.WriteLine($"your choice is {item}");
+        //         break;
+        //         case "fish" :
+        //         Console.WriteLine($"your choice is {item}");
+        //         break;
+        //         case "egg" :
+        //         Console.WriteLine($"your choice is {item}");
+        //         break;
+        //         default :
+        //         Console.WriteLine("This item is not available");
+        //         break;
+        //     }
+        //     break;
+        //     default :
+        //     Console.WriteLine("Please enter the exact word veg  or  non-veg");
+        //     break;
+        // }
+        // Note that the jump statement examples are given in notes with example.
+        // for is used when Known Number of Iterations are there , for  loop real world scenario example .
+        for (int employeeId = 1; employeeId <= 25; employeeId++)
+        {
+            Console.WriteLine($"Salary Slip sent to Employee {employeeId}");
         }
-    }
+        // there are exactly 500 employees whom we need to send the salary sleep.
+
+        // do-while used when we need to Execute code At Least Once , while loop real world scenario example .
+            int choice;
+            do
+            {
+                Console.WriteLine("\n===== ATM =====");
+                Console.WriteLine("1. Balance");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter Choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Balance: ₹50,000");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Deposit Selected");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Withdraw Selected");
+                        break;
+                }
+            } while (choice != 4);
+            Console.WriteLine("Thank You!");
+
+    // while – Unknown Number of Iterations Scenario: User Login
+    // A website keeps asking for credentials until the correct username and password are entered.
+
+            string username = "";
+            string password = "";
+
+            while (username != "Piyush" || password != "user@123")
+            {
+                Console.Write("Username: ");
+                username = Console.ReadLine();
+
+                Console.Write("Password: ");
+                password = Console.ReadLine();
+
+                if (username != "Piyush" || password != "user@123")
+                {
+                    Console.WriteLine("Invalid Credentials\n");
+                }
+            }
+
+            Console.WriteLine("Login Successful!");
+    } 
+
+    //  rest statement are given in notes with example .
+
 }
