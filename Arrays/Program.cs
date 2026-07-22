f90326610beffbd994d27b919d9f6329e1f6e816
Piyush Dubey <piyushdubey924@gmail.com>
2026-07-22 19:24:08 +0530

    arrays in C#

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program {
+    public static void Main(String []args){
+
+        // creating single dimentional arrays with different methods to know more please refer notes ..
+        int arr[] = new int[10];
+        
+        for(int i=0;i<)
+    }
+}
